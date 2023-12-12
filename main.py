36a4659a7acc5a3157e370e198e96f5ce67c9237
William Stone <118679388+will-s-stone@users.noreply.github.com>
2023-12-12 16:53:44 -0500

    Init.

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
+import os
+
 from openai import OpenAI
 client = OpenAI()
 
 temp = 0.2
-reps = 2
+reps = 1
 max_tokens = 64
 
 def perform_analysis():
     # Perform analysis for each model in the list
     # Perform according to the number of reps
-    models_list = open("files/models.txt").read().split("\n")
-    for model in models_list:
-        for x in range(reps):
-            completion = client.chat.completions.create(
-                model=model,
-                response_format={"type": "json_object"},
-                messages=[
-                    {"role": "system", "content": ""},
-                    {"role": "user", "content": task_file}
-                ],
-                temperature=temp,
-                max_tokens=max_tokens,
-            )
+    dir_path = "files/task_files"
+    for task_file in os.listdir(dir_path):
+        with open(os.path.join(dir_path, task_file)) as file:
+            task_file_name = task_file
+            task_file = file.read().replace('\n', '')
 
-            print(completion.choices[0].message)
+        models_list = open("files/models.txt").read().split("\n")
+        for model in models_list:
+            with open("files/responses/" + model.replace("-", "_") + "_" + str(temp) + "_" + task_file_name.replace(".txt", ".json"), 'a') as response_file:
+                for x in range(reps):
+                    completion = client.chat.completions.create(
+                        model=model,
+                        response_format={"type": "json_object"},
+                        messages=[
+                            {"role": "system", "content": ""},
+                            {"role": "user", "content": task_file}
+                        ],
+                        temperature=temp,
+                        max_tokens=max_tokens,
+                    )
+                    print(completion.choices[0].message.content)
+                    response_file.write(completion.choices[0].message.content)
+
 
 
 perform_analysis()

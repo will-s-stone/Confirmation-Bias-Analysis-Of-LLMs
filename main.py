357d3ff6a95faff1a429730fae737e79815687a7
William Stone <118679388+will-s-stone@users.noreply.github.com>
2023-12-11 19:06:43 -0500

    Init.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from openai import OpenAI
+client = OpenAI()
 
+temp = 0.2
+reps = 2
+max_tokens = 64
+
+def perform_analysis():
+    # Perform analysis for each model in the list
+    # Perform according to the number of reps
+    models_list = open("lib/models.txt").read().split("\n")
+    for model in models_list:
+        for x in range(reps):
+            completion = client.chat.completions.create(
+                model=model,
+                response_format={"type": "json_object"},
+                messages=[
+                    {"role": "system", "content": ""},
+                    {"role": "user", "content": task_file}
+                ],
+                temperature=temp,
+                max_tokens=max_tokens,
+            )
+
+            print(completion.choices[0].message)
+
+
+perform_analysis()

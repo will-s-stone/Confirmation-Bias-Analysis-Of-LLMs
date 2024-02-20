f854ff909e3440b81f51d69ad01618e63dafb7c3
William Stone <118679388+will-s-stone@users.noreply.github.com>
2024-02-20 00:14:19 -0500

    Changed method of passing in api key.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import os
 from dotenv import load_dotenv
@@ -6,8 +7,8 @@ from openai import OpenAI
 load_dotenv()
 client = OpenAI()
 
-temp = 0.0
-reps = 1
+temp = 0.7
+reps = 30
 max_tokens = 64
 
 def perform_tasks():
@@ -34,6 +35,28 @@ def perform_tasks():
                     print(completion.choices[0].message.content)
                     response_file.write(completion.choices[0].message.content)
 
+def write_results():
+    dir_path = "files/responses"
+    for file in os.listdir(dir_path):
+        response_json = json.load(open(os.path.join(dir_path, file)))
+        model_substring = '_'.join((file.split('_'))[2:-1])
+        task_substring = '_'.join((file.split('_'))[:2])
+        print(model_substring)
+        with open("files/results/" + model_substring + ".txt", 'a') as results_file:
+            log_path = "files/logical_conclusions"
+            correct_logic = json.load(open(os.path.join(log_path, "correct_logic/" + task_substring + ".json")))
+            affirming_consequent_logic = json.load(open(os.path.join(log_path, "affirming_consequent/" + task_substring + ".json")))
+            denying_antecedent_logic = json.load(open(os.path.join(log_path, "denying_antecedent/" + task_substring + ".json")))
+            if response_json == correct_logic:
+                results_file.write("On " + task_substring + " it did the correct logic\n")
+            elif response_json == affirming_consequent_logic:
+                results_file.write("On " + task_substring + " it affirmed the consequent\n")
+            elif response_json == denying_antecedent_logic:
+                results_file.write("On " + task_substring + " it denied the antecedent\n")
+            else:
+                results_file.write("On " + task_substring + " it was unpredictable\n")
+
+
 def perform_analysis():
     dir_path = "files/responses"
     for file in os.listdir(dir_path):
@@ -58,4 +81,3 @@ def perform_analysis():
 
 if __name__ == '__main__':
     perform_tasks()
-    perform_analysis()

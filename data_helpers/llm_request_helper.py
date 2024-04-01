2bfb1d10d53763212a3861eceba36114894d07c0
William Stone <118679388+will-s-stone@users.noreply.github.com>
2024-04-01 15:44:55 -0400

    Working on code revision.

--- data_helpers/llm_request_helper.py
+++ data_helpers/llm_request_helper.py
@@ -21,15 +21,46 @@ def get_gemini_response(cover_story, repetitions):
 
 
 def get_claude_response(cover_story, repetitions):
-    client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
-    message = client.messages.create(
-        model="claude-3-haiku-20240307",
-        max_tokens=1024,
-        messages=[
-            {"role": "user", "content": formatter.get_cover_story(1)}
-        ]
-    )
-    print(message.content)
+    for x in range(repetitions):
+        client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
+        message = client.messages.create(
+            model="claude-3-haiku-20240307",
+            max_tokens=1024,
+            messages=[
+                {"role": "user", "content": formatter.get_cover_story(1)}
+            ]
+        )
+        print(message.content)
+
+def get_gpt_3_5_response(cover_story, repetitions):
+    for x in range(repetitions):
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": ""},
+                {"role": "user", "content": task_file}
+            ],
+            temperature=temp,
+            max_tokens=max_tokens,
+        )
+        print(completion.choices[0].message.content)
+
+def get_gpt_4_response(cover_story, repetitions):
+    for x in range(repetitions):
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": ""},
+                {"role": "user", "content": cover_story}
+            ],
+            temperature=temp,
+            max_tokens=max_tokens,
+        )
+        print(completion.choices[0].message.content)
+
+
 
 
 

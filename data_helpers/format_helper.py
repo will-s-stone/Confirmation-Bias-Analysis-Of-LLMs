356adcdc02401220295d042258eae87b618507b4
will-s-stone <will@williamstone.me>
2024-04-01 20:17:58 -0400

    New vug involving .replace

--- data_helpers/format_helper.py
+++ data_helpers/format_helper.py
@@ -1,11 +1,11 @@
 import os
 import re
 import json
+import csv
 from datetime import datetime
 
 
 def get_cover_story(n):
-    cover_story_dir = "files/task_files"
     cover_story = ""
     if n > 5:
         raise ValueError("Cover Story Undefined")
@@ -25,24 +25,34 @@ def get_cover_story(n):
         case 5:
             print("task file five")
             cover_story = "task_5.txt"
-    with open(os.path.abspath(
-            os.path.join(os.path.join(os.pardir, os.path.join("files", "task_files")), cover_story))) as file:
+    file_path = os.path.abspath(
+        os.path.join(os.path.join(os.pardir, os.path.join("files", "task_files")), cover_story))
+    with open(file_path, 'r') as file:
         formatted_cover_story = file.read().replace('\n', '')
     return formatted_cover_story
 
 
-def get_json_substring(input_string):
+def _get_json_substring(input_string):
+    input_string = str(input_string)
     match = re.search(r'\{([^}]*)\}', input_string)
     if match:
-        return match.group(0).replace('\n', '')
+        result = str(match.group(0).replace('\n', '')).replace('\r', '').replace(',\n', ',')
+        print("final result: " + result)
+        return result
     return None
 
 
-def process_response(cover_story_number, input_string, model, temp):
+
+
+def _process_response(cover_story_number, input_string, model, temp):
     file_name = "task_" + str(cover_story_number)
-    response = get_json_substring(input_string)
-    json_resp = json.loads(response)
-    observation = [datetime.now(datetime.UTC).strftime("%Y-%m-%d~%H:%M:%S"), model, temp, file_name, 0, 0, 0, 0]
+    response = _get_json_substring(input_string).replace('\n', '')
+    print("PRESP: " + response)
+    response = response.replace('\n', '').replace('\r', '')
+    new_response = response.replace('{\n', '{')
+    print("AHHHHH new response line 53: " + new_response)
+    json_resp = json.loads(new_response)
+    observation = [datetime.utcnow().strftime("%Y-%m-%d~%H:%M:%S"), model, temp, file_name, 0, 0, 0, 0]
     if cover_story_number == 1:
         if json_resp["param1"].lower() == "yes":
             observation[4] = 1
@@ -111,3 +121,11 @@ def process_response(cover_story_number, input_string, model, temp):
     else:
         raise ValueError("Cover Story Number Error")
     return observation
+
+
+def log_observation(cover_story_number, response, model, temp):
+    observation = _process_response(cover_story_number, response, model, temp)
+    file_path = os.path.abspath(os.path.join(os.path.join(os.pardir, os.path.join("files", "results")), "results.csv"))
+    with open(file_path, 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(observation)
--- data_helpers/llm_request_helper.py
+++ data_helpers/llm_request_helper.py
@@ -17,60 +17,68 @@ openai_client = OpenAI(api_key=OPENAI_API_KEY)
 anthropic_client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
 
 
-def get_gemini_response(cover_story, repetitions, temp, max_tokens):
-    model = genai.GenerativeModel('gemini-pro')
-    generation_config = genai.GenerationConfig(temperature=temp, max_output_tokens= max_tokens)
+def get_gemini_response(cover_story_number, repetitions, temp, max_tokens):
+    model_name = 'gemini-pro'
+    model = genai.GenerativeModel(model_name)
+    generation_config = genai.GenerationConfig(temperature=temp, max_output_tokens=max_tokens)
     for x in range(repetitions):
-        response = model.generate_content(cover_story, generation_config=generation_config)
+        response = model.generate_content(formatter.get_cover_story(cover_story_number),
+                                          generation_config=generation_config)
         print(response.text)
+        formatter.log_observation(cover_story_number, response.text, model_name, temp)
 
 
-def get_claude_response(cover_story, repetitions, temp, max_tokens):
+def get_claude_response(cover_story_number, repetitions, temp, max_tokens):
+    model_name = "claude-3-haiku-20240307"
     for x in range(repetitions):
-        #client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
+        # client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
         message = anthropic_client.messages.create(
-            model="claude-3-haiku-20240307",
+            model=model_name,
             max_tokens=max_tokens,
             temperature=temp,
             messages=[
-                {"role": "user", "content": cover_story}
+                {"role": "user", "content": formatter.get_cover_story(cover_story_number)}
             ]
         )
         print(message.content)
+        formatter.log_observation(cover_story_number, message.content, model_name, temp)
 
 
-def get_gpt_3_5_response(cover_story, repetitions, temp, max_tokens):
+def get_gpt_3_5_response(cover_story_number, repetitions, temp, max_tokens):
+    model_name = "gpt-3.5-turbo-1106"
     for x in range(repetitions):
         completion = openai_client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
+            model=model_name,
             response_format={"type": "json_object"},
             messages=[
                 {"role": "system", "content": ""},
-                {"role": "user", "content": cover_story}
+                {"role": "user", "content": formatter.get_cover_story(cover_story_number)}
             ],
             temperature=temp,
             max_tokens=max_tokens,
         )
         print(completion.choices[0].message.content)
+        formatter.log_observation(cover_story_number, completion.choices[0].message.content, model_name, temp)
 
 
-def get_gpt_4_response(cover_story, repetitions, temp, max_tokens):
+def get_gpt_4_response(cover_story_number, repetitions, temp, max_tokens):
+    model_name = "gpt-3.5-turbo-1106"
     for x in range(repetitions):
         completion = openai_client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
+            model=model_name,
             response_format={"type": "json_object"},
             messages=[
                 {"role": "system", "content": ""},
-                {"role": "user", "content": cover_story}
+                {"role": "user", "content": formatter.get_cover_story(cover_story_number)}
             ],
             temperature=temp,
             max_tokens=max_tokens,
         )
         print(completion.choices[0].message.content)
+        formatter.log_observation(cover_story_number, completion.choices[0].message.content, model_name, temp)
 
 
-
-get_claude_response(formatter.get_cover_story(1), 2, 1, 1024)
-get_gpt_4_response(formatter.get_cover_story(1), 2, 1, 1024)
-get_gpt_3_5_response(formatter.get_cover_story(1), 2, 1, 1024)
-get_gemini_response(formatter.get_cover_story(1), 2, 1, 1024)
+get_claude_response(1, 2, 1, 1024)
+get_gpt_4_response(1, 2, 1, 1024)
+get_gpt_3_5_response(1, 2, 1, 1024)
+get_gemini_response(1, 2, 1, 1024)

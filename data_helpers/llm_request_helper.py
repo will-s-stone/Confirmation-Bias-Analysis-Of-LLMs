712637d7e772de7fdd981f2d0d4af6bd0b3d6d96
will-s-stone <will@williamstone.me>
2024-10-22 23:43:09 -0400

    Added in more local models

--- data_helpers/llm_request_helper.py
+++ data_helpers/llm_request_helper.py
@@ -1,85 +1,10 @@
+from openai import OpenAI
+
 import format_helper as formatter
 import os
-import dotenv
-import google.generativeai as genai
-import anthropic
-import openai
-from openai import OpenAI
 import time
 
-dotenv.load_dotenv()
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-# https://ai.google.dev/api/python/google/generativeai
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
-anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
-genai.configure(api_key=GOOGLE_API_KEY)
-openai_client = OpenAI(api_key=OPENAI_API_KEY)
-anthropic_client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
 lm_studio_client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
-
-
-def _get_gemini_response(cover_story_number, repetitions, temp, max_tokens):
-    model_name = 'gemini-pro'
-    model = genai.GenerativeModel(model_name)
-    generation_config = genai.GenerationConfig(temperature=temp, max_output_tokens=max_tokens)
-    for x in range(repetitions):
-        response = model.generate_content(formatter.get_cover_story(cover_story_number),
-                                          generation_config=generation_config)
-        print(response.text)
-        formatter.log_observation(cover_story_number, response.text, model_name, temp)
-
-
-def _get_claude_response(cover_story_number, repetitions, temp, max_tokens):
-    model_name = "claude-3-opus-20240229"
-    for x in range(repetitions):
-        # client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
-        message = anthropic_client.messages.create(
-            model=model_name,
-            max_tokens=max_tokens,
-            temperature=temp,
-            messages=[
-                {"role": "user", "content": formatter.get_cover_story(cover_story_number)}
-            ]
-        )
-        print(message.content)
-        # from message.content
-        formatter.log_observation(cover_story_number, message.json, model_name, temp)
-        time.sleep(12)
-
-
-def _get_gpt_3_5_response(cover_story_number, repetitions, temp, max_tokens):
-    model_name = "gpt-3.5-turbo-1106"
-    for x in range(repetitions):
-        completion = openai_client.chat.completions.create(
-            model=model_name,
-            response_format={"type": "json_object"},
-            messages=[
-                {"role": "system", "content": ""},
-                {"role": "user", "content": formatter.get_cover_story(cover_story_number)}
-            ],
-            temperature=temp,
-            max_tokens=max_tokens,
-        )
-        print(completion.choices[0].message.content)
-        formatter.log_observation(cover_story_number, completion.choices[0].message.content, model_name, temp)
-
-
-def _get_gpt_4_response(cover_story_number, repetitions, temp, max_tokens):
-    model_name = "gpt-4-turbo"
-    for x in range(repetitions):
-        completion = openai_client.chat.completions.create(
-            model=model_name,
-            response_format={"type": "json_object"},
-            messages=[
-                {"role": "system", "content": ""},
-                {"role": "user", "content": formatter.get_cover_story(cover_story_number)}
-            ],
-            temperature=temp,
-            max_tokens=max_tokens,
-        )
-        print(completion.choices[0].message.content)
-        formatter.log_observation(cover_story_number, completion.choices[0].message.content, model_name, temp)
 
 
 def _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, model_name):
@@ -116,23 +41,52 @@ def _get_phi_2_response(cover_story_number, repetitions, temp, max_tokens):
     _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, "phi-2")
 
 
+def _get_gemma_2_response(cover_story_number, repetitions, temp, max_tokens):
+    _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, "gemma-2-9b-it")
+
+
+def _get_stable_code_response(cover_story_number, repetitions, temp, max_tokens):
+    _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, "stable-code-instruct-3b")
+
+
+def _get_phi_3_1_response(cover_story_number, repetitions, temp, max_tokens):
+    _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, "phi-3.1-mini-128k-instruct")
+
+
+def _get_llama_3_2_response(cover_story_number, repetitions, temp, max_tokens):
+    _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, "llama-3.2-1b-instruct")
+
+
+def _get_internlm_response(cover_story_number, repetitions, temp, max_tokens):
+    _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, "internlm2_5-20b-chat")
+
+def _get_qwen2_1_5_response(cover_story_number, repetitions, temp, max_tokens):
+    _get_local_model_response(cover_story_number, repetitions, temp, max_tokens, "qwen2-0.5b-instruct")
+
+
 def get_non_local_responses(repetitions, temp, max_tokens_per_response):
     for x in range(5):
         x = x + 1
         # _get_gemini_response(x, repetitions, temp, max_tokens_per_response)
         # _get_claude_response(x, repetitions, temp, max_tokens_per_response)
         # _get_gpt_3_5_response(x, repetitions, temp, max_tokens_per_response)
-        _get_gpt_4_response(x, repetitions, temp, max_tokens_per_response)
+        #_get_gpt_4_response(x, repetitions, temp, max_tokens_per_response)
 
 
 def get_local_responses(repetitions, temp, max_tokens_per_response):
-    for x in range(5):
-        x = x + 1
+    #for x in range(5):
+        #x = x + 1
         # _get_qwen_1_5_response(x, repetitions, temp, max_tokens_per_response)
         # _get_wizard_coder_response(x, repetitions, temp, max_tokens_per_response)
         # _get_code_llama_response(x, repetitions, temp, max_tokens_per_response)
         # _get_mistral_response(x, repetitions, temp, max_tokens_per_response)
         # _get_phi_2_response(x, repetitions, temp, max_tokens_per_response)
+        #_get_gemma_2_response(x, repetitions, temp, max_tokens_per_response)
+        #_get_stable_code_response(x, repetitions, temp, max_tokens_per_response)
+        #_get_phi_3_1_response(x, repetitions, temp, max_tokens_per_response)
+        #_get_llama_3_2_response(x, repetitions, temp, max_tokens_per_response)
+        #_get_internlm_response(x, repetitions, temp, max_tokens_per_response)
+        _get_qwen2_1_5_response(5, repetitions, temp, max_tokens_per_response)
 
 
-get_non_local_responses(30, 0.5, 1024)
+get_local_responses(30, 0.5, 1024)
